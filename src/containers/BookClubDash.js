742ddd5e56cb6dda65b3003a0e06f200c40309cf
Dwire <gjdwyer8@gmail.com>
2019-04-05 11:43:34 -0400

    Refactor code to remove 2 components

--- src/containers/BookClubDash.js
+++ src/containers/BookClubDash.js
@@ -5,7 +5,17 @@ class BookClubDash extends Component {
   render() {
     return (
       <div>
-        Book Club Dash
+        <ul>
+          <li>Book Club Dash</li>
+          <li>GR Search</li>
+          <li>Members List</li>
+          <ul>
+            <li>top reviewed books</li>
+            <li>most read books</li>
+            <li>most recently added books</li>
+          </ul>
+          <li>Reading List for book club</li>
+        </ul>
       </div>
     );
   }
--- src/containers/UserBooks.js
+++ src/containers/UserBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import RenderUserBooks from '../components/RenderUserBooks'
+// import RenderUserBooks from '../components/RenderUserBooks'
+import RenderUserBookSvg from '../components/RenderUserBookSvg'
 
 class UserBooks extends Component {
   state = {
@@ -11,17 +12,26 @@ class UserBooks extends Component {
     this.setState({ownedBook: !this.state.ownedBook})
   }
 
+  renderBookSvgs = () => {
+    const bookList = this.state.ownedBook ? this.props.readBooks :  this.props.readingList
+     return bookList.map(book => <RenderUserBookSvg key={book.id} book={book}/>)
+  }
+
   render() {
     return (
       <div>
         <h1>{this.state.ownedBook ? "My Library" : "Reading List"}  <button onClick={this.handleClick} >{this.state.ownedBook ? "Future Books" : "Owned Books"}</button></h1>
-
-        <RenderUserBooks books={this.state.ownedBook ? this.props.readBooks : this.props.readingList} />
+        {this.renderBookSvgs()}
       </div>
     );
   }
 }
 
+
+UserBooks.defaultProps = {
+  readBooks: [],
+  readingList: []
+}
 
 const mapStateToProps = state => {
   return {
