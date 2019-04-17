2e33c1fcf7db1bfcc7bfd8dcc6f75a11e39f0f67
Dwire <gjdwyer8@gmail.com>
2019-04-17 17:07:18 -0400

    Refine svg image

--- src/components/RenderUserBookSvg.js
+++ src/components/RenderUserBookSvg.js
@@ -2,49 +2,31 @@ import React from 'react'
 
 const RenderUserBookSvg = ({book}) => {
   return (
-      <div>
-        <h4>{book.title}</h4>
-        <h6>{book.author.name}</h6>
-      </div>
+    <svg width="100%" height="100%" viewBox="0 0 4132 2471" version="1.1" xmlns="http://www.w3.org/2000/svg" >
+      <g>
+        <path d="M1503.26,789.27l-0.406,448.705l1928.6,1.747l0.406,-448.705l-1928.6,-1.747Z"/>
+        <path d="M1503.25,795.305l-0.395,436l1928.6,1.747l0.395,-436l-1928.6,-1.747Z" fill="#00602c" />
+        <path d="M1503.23,823.212l-0.345,380.953l1928.6,1.747l0.345,-380.953l-1928.6,-1.747Z" fill="url(#_Linear1)"/>
+        <path d="M1605.87,825.355l-0.345,380.953l53.573,0.049l0.345,-380.953l-53.573,-0.049Z" fill="url(#_Linear2)"/>
+        <path d="M1706.87,825.447l-0.345,380.952l53.573,0.049l0.345,-380.953l-53.573,-0.048Z" fill="url(#_Linear2)"/>
+        <text x="1816px" y="895.574px" font-size='80px'>Title: {book.title}</text>
+        <path d="M3181.01,824.66l-0.345,380.953l53.572,0.048l0.345,-380.952l-53.572,-0.049Z" fill="url(#_Linear2)"/>
+        <path d="M3282.01,824.752l-0.345,380.952l53.572,0.049l0.345,-380.953l-53.572,-0.048Z" fill="url(#_Linear2)"/>
+      </g>
+      <defs>
+        <linearGradient id="_Linear1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-272.495,-0.246659,0.246659,-272.495,3340.93,1207.83)">
+          <stop offset="0" stop-color="#00602c" stop-opacity="1"/>
+          <stop offset="1" stop-color="#40a629" stop-opacit="1"/>
+        </linearGradient>
+        <linearGradient id="_Linear2" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(0.0113714,-12.5589,12.5589,0.0113714,1603.86,837.916)">
+          <stop offset="0" stop-color="#665e10" stop-opacity="1"/>
+          <stop offset="1" stop-color="#d3c29b" stop-opacity="1"/>
+        </linearGradient>
+      </defs>
+    </svg>
   )
 }
 
 export default RenderUserBookSvg
 
 // <?xml version="1.0" encoding="UTF-8" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-// <svg width="100%" height="100%" viewBox="0 0 4132 2471" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
-//   <g>
-//     <path d="M1503.26,789.27l-0.406,448.705l1928.6,1.747l0.406,-448.705l-1928.6,-1.747Z"/>
-//     <path d="M1503.25,795.305l-0.395,436l1928.6,1.747l0.395,-436l-1928.6,-1.747Z" style="fill:#00602c;"/>
-//     <path d="M1503.23,823.212l-0.345,380.953l1928.6,1.747l0.345,-380.953l-1928.6,-1.747Z" style="fill:url(#_Linear1);"/>
-//     <path d="M1605.87,825.355l-0.345,380.953l53.573,0.049l0.345,-380.953l-53.573,-0.049Z" style="fill:url(#_Linear2);"/>
-//     <g>
-//       <path d="M1706.87,825.447l-0.345,380.952l53.573,0.049l0.345,-380.953l-53.573,-0.048Z" style="fill:url(#_Linear3);"/>
-//     </g>
-//     <text x="1816px" y="895.574px" style="font-family:'ArialMT', 'Arial', sans-serif;font-size:72px;">TEST<tspan x="1998.71px 2018.71px " y="895.574px 895.574px ">B</tspan>OOK</text>
-//       <path d="M3181.01,824.66l-0.345,380.953l53.572,0.048l0.345,-380.952l-53.572,-0.049Z" style="fill:url(#_Linear4);"/>
-//       <path d="M3282.01,824.752l-0.345,380.952l53.572,0.049l0.345,-380.953l-53.572,-0.048Z" style="fill:url(#_Linear5);"/>
-//   </g>
-//   <defs>
-//     <linearGradient id="_Linear1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-272.495,-0.246659,0.246659,-272.495,3340.93,1207.83)">
-//       <stop offset="0" style="stop-color:#00602c;stop-opacity:1"/>
-//       <stop offset="1" style="stop-color:#40a629;stop-opacity:1"/>
-//     </linearGradient>
-//     <linearGradient id="_Linear2" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(0.0113714,-12.5589,12.5589,0.0113714,1603.86,837.916)">
-//       <stop offset="0" style="stop-color:#665e10;stop-opacity:1"/>
-//       <stop offset="1" style="stop-color:#d3c29b;stop-opacity:1"/>
-//     </linearGradient>
-//     <linearGradient id="_Linear3" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(0.0113714,-12.5589,12.5589,0.0113714,1704.86,838.008)">
-//       <stop offset="0" style="stop-color:#665e10;stop-opacity:1"/>
-//       <stop offset="1" style="stop-color:#d3c29b;stop-opacity:1"/>
-//     </linearGradient>
-//     <linearGradient id="_Linear4" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(0.0113714,-12.5589,12.5589,0.0113714,3179,837.221)">
-//       <stop offset="0" style="stop-color:#665e10;stop-opacity:1"/>
-//       <stop offset="1" style="stop-color:#d3c29b;stop-opacity:1"/>
-//     </linearGradient>
-//     <linearGradient id="_Linear5" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(0.0113714,-12.5589,12.5589,0.0113714,3280,837.313)">
-//       <stop offset="0" style="stop-color:#665e10;stop-opacity:1"/>
-//       <stop offset="1" style="stop-color:#d3c29b;stop-opacity:1"/>
-//     </linearGradient>
-//   </defs>
-// </svg>
